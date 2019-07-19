4c7e450dbad1668c28f94a68abddc8498bb1d311
aszilagy <aszilagy@hawk.iit.edu>
2019-07-19 20:20:41 +0000

    home page template

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, abort
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template('index.html')
 
@@ -11,9 +11,18 @@ def logs():
         user = request.form['username']
         pa = request.form['password']
         if user == 'test' and pa == 'admin':
-            return "Logging"
+            return render_template('templateboot.html')
 
     return render_template('index.html', error='Wrong username/password.')
+
+@app.route('/post', methods=['GET', 'POST'])
+def apiPost():
+    if request.method == 'POST':
+        print(request.get_json())
+        return "200"
+
+    return abort(404)
+
         
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
