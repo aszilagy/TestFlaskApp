37ddc86f01824a44ae3272abc35e56e5f9f71efe
Szilagyi <xaxs251@campus.grainger.com>
2019-07-24 10:35:29 -0500

    new account creation

--- app.py
+++ app.py
@@ -1,19 +1,47 @@
-from flask import Flask, render_template, request, abort
+from flask import Flask, render_template, request, abort, redirect, url_for
 app = Flask(__name__)
 
+#FIXME: Move this to mongodb or similar DB
+userDict = {'test':'admin'}
+
 @app.route('/', methods=['GET', 'POST'])
-def home():
+def login():
     return render_template('index.html')
 
 @app.route('/home', methods=['GET','POST'])
-def logs():
+def home():
     if request.method == 'POST':
+        print("Request form:",request.form)
+        #if 'username' not in request.form or 'password' not in request.form:
+        #    return render_template('index.html')
+
         user = request.form['username']
         pa = request.form['password']
-        if user == 'test' and pa == 'admin':
-            return render_template('templateboot.html')
+        if user in userDict:
+            if userDict[user] == pa:
+                print("Logging into test account")
+                return render_template('templateboot.html')
 
     return render_template('index.html', error='Wrong username/password.')
+
+@app.route('/createAccount', methods=['POST'])
+def createAccount():
+    print("Hitting accountCreate")
+    if request.method == 'POST':
+        user = request.form['newUsername']
+        pa = request.form['newPassword']
+        email = request.form['newEmail']
+
+        if user not in userDict:
+            userDict[user] = pa
+            print("Creating user: %s %s %s" % (user, pa, email))
+
+        else:
+            print("User already exists")
+
+        #return redirect(url_for('home'))
+        return "abc"
+    
 
 @app.route('/registration', methods=['GET'])
 def register():
